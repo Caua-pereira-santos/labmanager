2f1053a40ad12993fd3f079dbca5527d82372beb
caua-pereira-santos <cauap600@gmail.com>
2022-05-17 14:43:38 -0300

    refactor: create database setup class

---
+++ Database/DatabaseSetup.cs
@@ -0,0 +1,52 @@
+namespace LabManager.Darabase;
+
+class DatabaseSetup
+{
+    private DatabaseConfig databaseConfig;
+
+    public DatabaseSetup(DatabaseConfig databaseConfig)
+    {
+        this.databaseConfig = databaseConfig;
+        CreateComputerTable();
+    }
+    public void CreateComputerTable()
+    {
+        connection.Open();
+
+var command = connection.CreateCommand();
+command.CommandText = @"
+
+CREATE TABLE IF NOT EXISTS Computers(
+    id int not null primary key,
+    ram varchar(100) not null,
+    processor varchar(100) not null
+);
+
+
+
+";
+
+var command = connection.CreateCommand();
+command.CommandText = @"
+
+CREATE TABLE IF NOT EXISTS Lab(
+    id int not null primary key,
+    number varchar(100) not null,
+    name varchar(100) not null,
+    block varchar(100) not null);
+    
+    
+    ";
+
+
+command.ExecuteNonQuery();
+
+connection.Close();
+    }
+}
+
+
+
+
+
+
